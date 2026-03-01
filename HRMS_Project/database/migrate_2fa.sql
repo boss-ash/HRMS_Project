c65af650dc20eb73b7274ed39e96a559536eb062
Boss_A <kyutrobloxpopo@gmail.com>
2026-03-02 02:01:30 +0900

    Delete HRMS_Project directory

--- HRMS_Project/database/migrate_2fa.sql
+++
@@ -1,3 +0,0 @@
--- Add Google Authenticator (2FA) secret to users. Run once.
-USE hrms_db;
-ALTER TABLE users ADD COLUMN totp_secret VARCHAR(255) NULL DEFAULT NULL;
--- HRMS_Project/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HRMS_LOADED', true);
-require_once __DIR__ . '/includes/auth.php';
-if (isLoggedIn()) {
-    header('Location: dashboard.php');
-} else {
-    header('Location: login.php');
-}
-exit;
